tomitribe/activemq

commit 019b3f93683214afbceb29dcdad9a1f68d5926b6

Adding missing jsp escapes (#2089)

activemq-web-console/src/main/webapp/browse.jsp
@@ -48,7 +48,7 @@
 <td><a href="<c:url value="message.jsp">
                  <c:param name="id" value="${row.JMSMessageID}" />
                  <c:param name="JMSDestination" value="${requestContext.queueBrowser.JMSDestination}"/></c:url>"
-    title="<c:out value="${row.properties}"/>">${row.JMSMessageID}</a></td>
+    title="<c:out value="${row.properties}"/>"><c:out value="${row.JMSMessageID}"/></a>
 <td><c:out value="${row.JMSCorrelationID}"/></td>
 <td><jms:persistent message="${row}"/></td>
 <td><c:out value="${row.JMSPriority}"/></td>

activemq-web-console/src/main/webapp/queueConsumers.jsp
@@ -60,7 +60,7 @@
 	<td>
         <c:choose>
             <c:when test="${row.network}">
-                <a href="network.jsp">${row.clientId}</a><br/>
+                <a href="network.jsp"><c:out value="${row.clientId}"/></a><br/>
             </c:when>
             <c:otherwise>
                 <a href="<c:out value="connection.jsp?connectionID=${row.clientId}"/>"><c:out value="${row.clientId}" /></a><br/>
@@ -90,4 +90,3 @@
 
 </body>
 </html>
-	

commit 191b3ca68399b04df3e23f04889725bf2c711fdf

CVE-2026-52760: Add back missing closing tag (#2100) (#2106)
Fix typo introduced in #2089

(cherry picked from commit 83ce2bacd84edfea4282132aff64f49ecb6baefd)
(cherry picked from commit 7e6705f9821aa6da2e025c684fea26701025a6ae)

activemq-web-console/src/main/webapp/browse.jsp
@@ -50,7 +50,7 @@
 <td><a href="<c:url value="message.jsp">
                  <c:param name="id" value="${row.JMSMessageID}" />
                  <c:param name="JMSDestination" value="${requestContext.queueBrowser.JMSDestination}"/></c:url>"
-    title="<c:out value="${row.properties}"/>"><c:out value="${row.JMSMessageID}"/></a>
+    title="<c:out value="${row.properties}"/>"><c:out value="${row.JMSMessageID}"/></a></td>
 <td><c:out value="${row.JMSCorrelationID}"/></td>
 <td><jms:persistent message="${row}"/></td>
 <td><c:out value="${row.JMSPriority}"/></td>